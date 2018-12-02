ddf1be982b056fb4d1432e86b7b053ef37badabf
Danex2 <khadane.miller@gmail.com>
2018-12-01 19:31:21 -0500

    can now check ranks in different regions

--- index.js
+++ index.js
@@ -14,48 +14,67 @@ function winPC(league) {
   return win.toFixed(1);
 }
 
+const regions = {
+  na: "na1",
+  ru: "ru",
+  kr: "kr",
+  br: "br1",
+  oc: "oc1",
+  eune: "eun1",
+  euw: "euw1",
+  tr: "tr1",
+  lan: "la1",
+  las: "la2"
+};
+
 client.on("message", message => {
   messageArray = message.content.split(" ");
   [command, summoner, region] = [...messageArray];
-  if (command === `${prefix}rank`) {
-    api.get("na1", "summoner.getBySummonerName", summoner).then(data =>
-      api
-        .get("na1", "league.getAllLeaguePositionsForSummoner", data.id)
-        .then(league =>
-          message.channel.send({
-            embed: {
-              color: 3447003,
-              author: {
-                name: summoner,
-                icon_url: client.user.avatarURL
-              },
-              title: `Ranked stats for ${summoner}`,
-              fields: [
-                {
-                  name: "League name",
-                  value: league[0].leagueName
+  if (command === `${prefix}rank` && summoner !== "" && region !== "") {
+    api
+      .get(regions[region], "summoner.getBySummonerName", summoner)
+      .then(data =>
+        api
+          .get(
+            regions[region],
+            "league.getAllLeaguePositionsForSummoner",
+            data.id
+          )
+          .then(league =>
+            message.channel.send({
+              embed: {
+                color: 3447003,
+                author: {
+                  name: summoner,
+                  icon_url: client.user.avatarURL
                 },
-                {
-                  name: "Tier",
-                  value: `${league[0].tier} ${league[0].rank}`
-                },
-                {
-                  name: "League points",
-                  value: league[0].leaguePoints
-                },
-                {
-                  name: "Wins / Losses (%)",
-                  value: `${league[0].wins} / ${league[0].losses} %(${winPC(
-                    league
-                  )})`
-                }
-              ],
-              timestamp: new Date()
-            }
-          })
-        )
-        .catch(err => console.log(err))
-    );
+                title: `Ranked stats for ${summoner}`,
+                fields: [
+                  {
+                    name: "League name",
+                    value: league[0].leagueName
+                  },
+                  {
+                    name: "Tier",
+                    value: `${league[0].tier} ${league[0].rank}`
+                  },
+                  {
+                    name: "League points",
+                    value: league[0].leaguePoints
+                  },
+                  {
+                    name: "Wins / Losses (%)",
+                    value: `${league[0].wins} / ${league[0].losses} %(${winPC(
+                      league
+                    )})`
+                  }
+                ],
+                timestamp: new Date()
+              }
+            })
+          )
+          .catch(err => console.log(err))
+      );
   }
 });
 
